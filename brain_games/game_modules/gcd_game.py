da38825cbdf9494d8abcfeb3a49b5198e87755eb
dosegaev <dosegaevka@gmail.com>
2022-11-28 12:15:44 +0800

    fix gcd_game

--- brain_games/game_modules/gcd_game.py
+++ brain_games/game_modules/gcd_game.py
@@ -8,6 +8,6 @@ OBJECTIVE = 'Find the greatest common divisor of given numbers.'
 def get_results():
     first_value = randint(1, 100)
     second_value = randint(1, 100)
-    question = (f'{first_value}, {second_value}')
+    question = (f'{first_value} {second_value}')
     correct_answer = str(math.gcd(first_value, second_value))
     return question, correct_answer
