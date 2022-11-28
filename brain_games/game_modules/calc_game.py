1a289733cda61467cf44c363d1c53f0377eb42b2
dosegaev <dosegaevka@gmail.com>
2022-11-28 12:25:44 +0800

    fix calc_game

--- brain_games/game_modules/calc_game.py
+++ brain_games/game_modules/calc_game.py
@@ -8,7 +8,7 @@ OBJECTIVE = 'What is the result of the expression?'
 def get_results():
     first_value = randint(1, 10)
     second_value = randint(1, 10)
-    operator = choice(['-', '+', '/', '*', ])
+    operator = choice(['-', '+', '*', ])
     question = (f'{first_value} {operator} {second_value}')
     correct_answer = str(calc(first_value, second_value, operator))
     return question, correct_answer
@@ -19,8 +19,6 @@ def calc(first_value, second_value, operator):
         result = (first_value - second_value)
     elif operator == '+':
         result = (first_value + second_value)
-    elif operator == '/':
-        result = int(first_value / second_value)
     elif operator == '*':
         result = int(first_value * second_value)
     return result
